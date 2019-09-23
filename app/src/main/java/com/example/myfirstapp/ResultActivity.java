9297ac6dd881f4f139abc31d206adb4e53a66760
Valavan <valavan.rajan@gmail>
2019-09-23 17:30:35 +0800

    Edited using current date and time

--- app/src/main/java/com/example/myfirstapp/ResultActivity.java
+++ app/src/main/java/com/example/myfirstapp/ResultActivity.java
@@ -22,6 +22,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 
 public class ResultActivity extends AppCompatActivity {
     private TextView mTextViewResult;
@@ -67,18 +68,42 @@ public class ResultActivity extends AppCompatActivity {
                             String fullAddress = recordsObj.getString("address");
                             String freeParking = recordsObj.getString("free_parking");
 
-                            String resultString = "Free parking availability: " + freeParking + "\n";
-                            String pricing;
-                            if(Arrays.asList(centralCarparks).contains(carParkNo)){
-                                pricing = "$1.20 per half hour Mondays to Saturdays 7am to 5pm. $0.60 other hours\n\n";
+                            //String resultString = "Free parking availability: " + freeParking + "\n";
+                            String pricing="";
+                            Calendar calendar = Calendar.getInstance();
+                            int day = calendar.get(Calendar.DAY_OF_WEEK);
+                            switch (day){
+                                case Calendar.SUNDAY:
+                                    if (freeParking.equals("NO")){
+                                        pricing = "$0.60 per half hour\n\n";
+                                        break;
+                                    }
+                                    else if (calendar.get(Calendar.HOUR_OF_DAY)>6 && (calendar.get(Calendar.HOUR_OF_DAY)<22 || (calendar.get(Calendar.HOUR_OF_DAY)==22 && calendar.get(Calendar.MINUTE)<=30 ))){
+                                        pricing = "Current pricing is free!";
+                                        break;
+                                    }
+                                    else
+                                        pricing = "$0.60 per half hour\n\n";
+                                        break;
+                                case Calendar.MONDAY:
+                                case Calendar.TUESDAY:
+                                case Calendar.WEDNESDAY:
+                                case Calendar.THURSDAY:
+                                case Calendar.FRIDAY:
+                                case Calendar.SATURDAY:
+                                    if(Arrays.asList(centralCarparks).contains(carParkNo) && calendar.get(Calendar.HOUR_OF_DAY)>6 && (calendar.get(Calendar.HOUR_OF_DAY)<17)){
+                                        pricing = "$1.20 per half hour\n\n";
+                                        break;
+                                    }
+                                    else
+                                        pricing = "$0.60 per half hour\n\n";
+                                        break;
                             }
-                            else
-                                pricing = "$0.60 per half hour\n\n";
 
 
                             textView.setText(fullAddress);
 
-                            mTextViewResult.append(resultString);
+                            //mTextViewResult.append(resultString);
                             mTextViewResult.append(pricing);
                             searchForAvailability(carParkNo);
                         } catch (JSONException e) {
